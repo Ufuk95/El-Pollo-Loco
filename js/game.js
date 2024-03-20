3dbc6c2c933c83a6d8d5653c45181927b6e3b88b
Ufuk Özsahin <oezsahin95@icloud.com>
2024-03-20 11:33:56 +0100

    first draw

--- js/game.js
+++ js/game.js
@@ -1,12 +1,11 @@
 let canvas;
-let ctx;
-let world = new World();
+let world;
 
 
 
 function init(){
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
     console.log('my character is ', world.character)
     console.log('my enemie is ', world.enemies)
