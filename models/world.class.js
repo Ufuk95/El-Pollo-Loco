49fa542bb87de0aa993e1b844930505284e5c0c4
Ufuk Özsahin <oezsahin95@icloud.com>
2024-03-21 16:14:46 +0100

    finishing background

--- models/world.class.js
+++ models/world.class.js
@@ -5,15 +5,49 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    backgroundObjects = [
+        new BackgroundObject('../img/5_background/layers/air.png', 0),
+        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0)
+    ];
     ctx;
+    canvas;
+    clouds = [
+        new Cloud()
+    ]
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
     draw(){
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        
+        
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addToMap(this.character);
+
+        // draw() wird immer wieder aufgerufen 
+        // in der requestAnimationFrame kennt er kein 'this'
+        let self = this
+        requestAnimationFrame(function(){
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(object){
+        object.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
