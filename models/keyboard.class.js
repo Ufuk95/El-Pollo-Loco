138847c1707941cc36759737892b5872caa97197
Ufuk Özsahin <oezsahin95@icloud.com>
2024-05-08 10:07:16 +0200

    smartphone finished

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -5,4 +5,102 @@ class Keyboard {
     DOWN = false;
     SPACE = false;
     D = false;
+
+
+
+
+    constructor(){
+        this.KeyboardControll();
+        this.touchControll();
+    }
+
+    KeyboardControll(){
+        window.addEventListener('keydown', (event) => {
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = true;
+            }
+            if (event.keyCode == 37) {
+                keyboard.LEFT = true;
+            }
+            if (event.keyCode == 38) {
+                keyboard.UP = true;
+            }
+            if (event.keyCode == 40) {
+                keyboard.DOWN = true;
+            }
+            if (event.keyCode == 32) {
+                keyboard.SPACE = true;
+            }
+            if (event.keyCode == 68) {
+                keyboard.D = true;
+            }
+        })
+        
+        window.addEventListener('keyup', (event) => {
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = false;
+            }
+            if (event.keyCode == 37) {
+                keyboard.LEFT = false;
+            }
+            if (event.keyCode == 38) {
+                keyboard.UP = false;
+            }
+            if (event.keyCode == 40) {
+                keyboard.DOWN = false;
+            }
+            if (event.keyCode == 32) {
+                keyboard.SPACE = false;
+            }
+            if (event.keyCode == 68) {
+                keyboard.D = false;
+            }
+        
+        })
+    }
+
+
+
+    touchControll() {
+        document.getElementById('moveLeftTouch').addEventListener('touchstart', (e) => {
+          e.preventDefault();
+          this.LEFT = true;
+        });
+    
+        document.getElementById('moveLeftTouch').addEventListener('touchend', (e) => {
+          e.preventDefault();
+          this.LEFT = false;
+        });
+    
+        document.getElementById('moveRightTouch').addEventListener('touchstart', (e) => {
+          e.preventDefault();
+          this.RIGHT = true;
+        });
+    
+        document.getElementById('moveRightTouch').addEventListener('touchend', (e) => {
+          e.preventDefault();
+          this.RIGHT = false;
+        });
+    
+        document.getElementById('jumpTouch').addEventListener('touchstart', (e) => {
+          e.preventDefault();
+          this.SPACE = true;
+        });
+    
+        document.getElementById('jumpTouch').addEventListener('touchend', (e) => {
+          e.preventDefault();
+          this.SPACE = false;
+        });
+    
+        document.getElementById('bottleTouch').addEventListener('touchstart', (e) => {
+          e.preventDefault();
+          this.D = true;
+        });
+    
+        document.getElementById('bottleTouch').addEventListener('touchend', (e) => {
+          e.preventDefault();
+          this.D = false;
+        });
+    
+      }
 }
